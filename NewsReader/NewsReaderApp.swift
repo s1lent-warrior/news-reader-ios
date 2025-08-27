a022ecff20accded1fb82c1c8d75821005aaf481
Muneeb Ahmed Anwar <muneeb.ahmed.anwar@gmail.com>
2025-08-27 14:48:50 +0500

    Initial Commit

---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by Muneeb Ahmed Anwar on 27/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsReaderTests/NewsReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderTests.swift
+//  NewsReaderTests
+//
+//  Created by Muneeb Ahmed Anwar on 27/08/2025.
+//
+
+import Testing
+@testable import NewsReader
+
+struct NewsReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
